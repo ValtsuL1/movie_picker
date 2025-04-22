2f76160f27491e625e76fb3112ad62e80f42cca0
ValtsuL1 <linnavaltteri@gmail.com>
2025-04-22 14:10:47 +0300

    added flutter_dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:movie_picker/pages/my_home_page.dart';
 import 'package:movie_picker/providers/my_app_state.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+Future<void> main() async {
+
+  await dotenv.load(fileName: ".env");
+
   runApp(MyApp());
 }
 
